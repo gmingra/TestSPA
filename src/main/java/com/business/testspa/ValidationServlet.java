892e82865caab9e5519b34a8e1b5c165926c18c4
giuseppe <giuseppemaria.ingra@outlook.it>
2021-11-12 19:10:05 +0100

    New Commit Again

--- src/main/java/com/business/testspa/ValidationServlet.java
+++ src/main/java/com/business/testspa/ValidationServlet.java
@@ -1,21 +1,15 @@
 package com.business.testspa;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "ValidationServlet", value = "/validation")
 public class ValidationServlet extends HttpServlet {
-    private String message;
-
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-
-        response.setContentType("application/xml");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         int id = Integer.parseInt(request.getParameter("id"));
         String description = request.getParameter("description");
@@ -23,34 +17,26 @@ public class ValidationServlet extends HttpServlet {
 
         response.setContentType("application/xml");
         PrintWriter out = response.getWriter();
+
         StringBuffer sb = new StringBuffer();
+
         sb.append("<response>");
 
-        if(id<100)
+        if (id <100)
             sb.append("<msg1>id &gt; 100</msg1>");
 
-        if("".equals(description))
-            sb.append("<msg2>description mandatory</smg2>");
+        if ("".equals(description))
+            sb.append("<msg2>description mandatory</msg2>");
 
-        if(amount>=1000 || amount>=2000)
-            sb.append("<msg3>import &gt; 1000 and import &lt; 2000</msg3>");
-
-
+        if (amount<=1000 || amount>=2000)
+            sb.append("<msg3>amount &gt; 1000 and amount &lt; 2000</msg3>");
 
         sb.append("</response>");
 
         out.println(sb.toString());
 
-
-        System.out.println("in doGet()");
-
         out.close();
-        System.out.println("Version final today");
-
-
-
-
+        System.out.println("version final today");
     }
-
-
 }
+
